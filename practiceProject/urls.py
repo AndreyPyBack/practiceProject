3ceced799f33de4bc9ddaec562e99db6c75bff4f
Simik <simik101297@gmail.com>
2023-10-02 18:02:20 +0600

    bleaaaaa

--- practiceProject/urls.py
+++ practiceProject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vlad/', include('vlad.urls')),
+    path('simik/', include('simik.urls'), name='simik'),
 ]
